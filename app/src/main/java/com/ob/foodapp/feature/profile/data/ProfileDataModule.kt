8507a340ef2cc95bef1e78aa5a60542436f6ba6e
Anibal Bastias Soto <anibal.bastias@gmail.com>
2021-08-18 21:22:00 -0400

    Minor fix for Kodein related with Firestore singleton injection

--- app/src/main/java/com/ob/foodapp/feature/profile/data/ProfileDataModule.kt
+++ app/src/main/java/com/ob/foodapp/feature/profile/data/ProfileDataModule.kt
@@ -1,10 +1,8 @@
 package com.ob.foodapp.feature.profile.data
 
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
+import com.ob.foodapp.feature.profile.MODULE_NAME
 import com.ob.foodapp.feature.profile.data.repository.RemoteProfileRepositoryImpl
 import com.ob.foodapp.feature.profile.domain.repository.RemoteProfileRepository
-import com.ob.foodapp.feature.profile.MODULE_NAME
 import org.kodein.di.Kodein
 import org.kodein.di.generic.bind
 import org.kodein.di.generic.instance
@@ -12,7 +10,7 @@ import org.kodein.di.generic.singleton
 
 internal val dataModule = Kodein.Module("${MODULE_NAME}DataModule") {
 
-    bind() from singleton { Firebase.firestore }
+//    bind() from singleton { Firebase.firestore }
 
     bind<RemoteProfileRepository>() with singleton {
         RemoteProfileRepositoryImpl(
